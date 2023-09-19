0e35f918e495859e95ac035ce595968f3cadd9c2
arielmartin5 <arielmartin5@gmail.com>
2023-09-19 18:24:09 +0000

    ppp

--- src/workflow-inicial/651_HT_lightgbm_PROBL07.r
+++ src/workflow-inicial/651_HT_lightgbm_PROBL07.r
@@ -500,7 +500,7 @@ dataset[, clase01 := ifelse(clase_ternaria == "CONTINUA", 0L, 1L)]
 campos_buenos <- setdiff(
   copy(colnames(dataset)),
   c("clase01", "clase_ternaria", "fold_train", "fold_validate", "fold_test", 
-    "numero_de_clientes", "foto_mes")
+    "numero_de_cliente", "foto_mes")
 )
 
 # la particion de train siempre va
